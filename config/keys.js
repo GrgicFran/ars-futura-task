c158974313e3d2094b4bc48ff2dffed02aa01723
Fran Grgić <grgicfran@gmail.com>
2019-09-25 13:19:52 +0200

    add mongo db, separate dev and prod environments

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// keys.js -- figure out which set of keys to return
+if (process.env.NODE_ENV === "production") {
+   // in production
+   module.exports = require("./prod");
+} else {
+   // in development
+   module.exports = require("./dev");
+}
