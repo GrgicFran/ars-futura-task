0ee1c2dfd5c91c98b543276daa5e0d7f31762874
Fran Grgić <grgicfran@gmail.com>
2019-09-26 13:34:04 +0200

    style fixes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
    renderContent() {
@@ -29,20 +30,22 @@ class Header extends Component {
    render() {
       return (
          <nav className="flex items-center justify-between flex-wrap bg-blue-900 p-6">
-            <div className="flex items-center flex-shrink-0 text-white mr-6">
-               <svg
-                  class="fill-current h-8 w-8 mr-2"
-                  width="54"
-                  height="54"
-                  viewBox="0 0 54 54"
-                  xmlns="http://www.w3.org/2000/svg"
-               >
-                  <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-               </svg>
-               <span className="font-semibold text-xl tracking-tight">
-                  Kalendarko
-               </span>
-            </div>
+            <Link to={this.props.auth ? "/calendar" : "/"}>
+               <div className="flex items-center flex-shrink-0 text-white mr-6">
+                  <svg
+                     class="fill-current h-8 w-8 mr-2"
+                     width="54"
+                     height="54"
+                     viewBox="0 0 54 54"
+                     xmlns="http://www.w3.org/2000/svg"
+                  >
+                     <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+                  </svg>
+                  <span className="font-semibold text-xl tracking-tight">
+                     Kalendarko
+                  </span>
+               </div>
+            </Link>
             <div>{this.renderContent()}</div>
          </nav>
       );
